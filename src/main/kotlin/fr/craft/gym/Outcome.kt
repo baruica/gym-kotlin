8cae9fe4adf7becf5ec5bb0a02c9b1af0faf9e9a
Nelson da Costa <ndacosta@octo.com>
2020-04-17 14:34:47 +0200

    delete Outcome.kt

--- src/main/kotlin/fr/craft/gym/Outcome.kt
+++
@@ -1,6 +0,0 @@
-package fr.craft.gym
-
-sealed class Outcome<out T : Any> {
-    data class Success<out T : Any>(val value: T) : Outcome<T>()
-    data class Error(val message: String, val cause: Exception? = null) : Outcome<Nothing>()
-}
