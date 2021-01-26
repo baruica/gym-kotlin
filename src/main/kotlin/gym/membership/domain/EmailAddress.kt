741e1a1817696d83932995bccc64a52ef4fa52c8
Nelson <baruica@gmail.com>
2021-01-26 08:25:39 +0100

    remove useless toString() override inline regex string

--- src/main/kotlin/gym/membership/domain/EmailAddress.kt
+++ src/main/kotlin/gym/membership/domain/EmailAddress.kt
@@ -3,16 +3,9 @@ package gym.membership.domain
 import kotlin.text.RegexOption.IGNORE_CASE
 
 data class EmailAddress(val value: String) {
-
-    private val pattern = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$"
-
     init {
-        require(Regex(pattern, IGNORE_CASE).matches(value)) {
+        require(Regex("^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$", IGNORE_CASE).matches(value)) {
             "EmailAddress must be a valid email address, was [$value]"
         }
-    }
-
-    override fun toString(): String {
-        return value
     }
 }
