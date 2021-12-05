b684b504a9aecb2bc0cacb513783283471ffd874
Nelson <baruica@gmail.com>
2021-12-05 10:00:45 +0100

    chore: remove detekt and update kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.0"
     id("io.kotest") version "0.3.8"
-    id("io.gitlab.arturbosch.detekt") version "1.18.1"
 }
 
 group = "me.baruica"
@@ -21,13 +20,13 @@ tasks.withType<Test> {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.0.0")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.0.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.1")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
 
 tasks.withType<JavaCompile> {
