28a1be776eac84077c5f819656447987d580090a
Nelson <baruica@gmail.com>
2022-03-13 22:17:33 +0100

    refactor: EmailAddress becomes a value class

--- src/main/kotlin/gym/membership/domain/EmailAddress.kt
+++ src/main/kotlin/gym/membership/domain/EmailAddress.kt
@@ -2,10 +2,15 @@ package gym.membership.domain
 
 import kotlin.text.RegexOption.IGNORE_CASE
 
-data class EmailAddress(val value: String) {
+@JvmInline
+value class EmailAddress(val value: String) {
+
     init {
-        require(Regex("^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$", IGNORE_CASE).matches(value)) {
+        require(isValidEmail(value)) {
             "EmailAddress must be a valid email address, was [$value]"
         }
     }
+
+    private fun isValidEmail(value: String) =
+        Regex("^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$", IGNORE_CASE).matches(value)
 }
