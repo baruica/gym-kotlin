d372b2ff2a394f1fe73ef638e51005100a6a4caa
Nelson <baruica@gmail.com>
2022-07-25 07:12:55 +0200

    chore: upgrade kotest to 5.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.3.2")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.2")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.4.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.4.0")
 }
 
 java {
