897f630c8a2ef031ea2ea99129e6cb53ad99cdcd
Nelson da Costa <baruica@gmail.com>
2023-01-03 11:41:43 +0100

    chore: remove kotlin-stdlib-xxx because not needed anymore since kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.4")
